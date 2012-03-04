d63ee6037539d81934d9ee18ef849ed9cd4963d1
Max Mueller <muellmax36@gmail.com>
2012-03-03 22:58:01 -0800

    updated registration file to reflect register form.

--- registration/index.php
+++ registration/index.php
@@ -2,41 +2,47 @@
 <?php include '../header.htm' ?>
 
 <p>Please register for an account and have access to everything Buhrinc has to offer.</p>
-
-
-	<fieldset class="fieldsetfloat">
-		<legend>   </legend>
-		
-		<label for="username">Choose A Username*</label>
-		<input for="username" id="username" type="text" accesskey="u" tabindex="5" />
-		<span class="example">min of 6 chars</span></p>
-		
-		<label for="password">Choose A Password*</label>
-		<input for="password" id="password" type="text" accesskey="p" tabindex="6" />
-		<span class="example">min of 6 chars</span>
-		
-		<label for="confirm">Confirm Password*</label>
-		<input for="confirm" id="username" type="text" accesskey="c" tabindex="7" />
-		
-	</fieldset>
-	
+<form id='register' action='register.php' method='post'
+	accept-charset='UTF-8'>
 	<fieldset>
-		<legend>Please enter registration information.</legend>
-		
-		<label for="firstname">First Name*</label>
-		<input name="firstname" id="firstname" type="text" accesskey="f" tabindex="1" />
-		
-		<label for="lastname">Last Name*</label>
-		<input name="lastname" id="lastname" type="text" accesskey="l" tabindex="2" />
-		
-		<label for="email">Email*</label>
-		<input name="email" id="email" type="text" accesskey="e" tabindex="3" />
-		
-		<label for="phone">Phone Number</label>
-		<input name="phone" id="phone" type="text" accesskey="n" tabindex="4" />
-		
+		<legend>Register</legend>
+		<input type='hidden' name='submitted' id='submitted' value='1' />
+		<label for='firstname' >Your First Name*: </label>
+		<input type='text' name='firstname' id='firstname' maxlength="50" />
+		<label for='lastname' >Your Last Name*: </label>
+		<input type='text' name='lastname' id='lastname' maxlength="50" />
+		<label for='email' >Email Address*:</label>
+		<input type='text' name='email' id='email' maxlength="50" />
+		<label for='username' >UserName*:</label>
+		<input type='text' name='username' id='username' maxlength="50" />
+		<label for='password' >Password*:</label>
+		<input type='password' name='password' id='password' maxlength="50" />
+		<input type='submit' name='Submit' value='Submit' />
 	</fieldset>
-	
-	
+</form>
 
+<!-- client-side Form Validations:
+Uses the excellent form validation script from JavaScript-coder.com-->
+<script type='text/javascript'>
+// <![CDATA[
+    var pwdwidget = new PasswordWidget('thepwddiv','password');
+    pwdwidget.MakePWDWidget();
+    
+    var frmvalidator  = new Validator("register");
+    frmvalidator.EnableOnPageErrorDisplay();
+    frmvalidator.EnableMsgsTogether();
+    frmvalidator.addValidation("firstname","req","Please provide your first name");
+	
+	frmvalidator.addValidation("lastname","req","Please provide your last name");
+	
+    frmvalidator.addValidation("email","req","Please provide your email address");
+
+    frmvalidator.addValidation("email","email","Please provide a valid email address");
+
+    frmvalidator.addValidation("username","req","Please provide a username");
+    
+    frmvalidator.addValidation("password","req","Please provide a password");
+
+// ]]>
+</script>
 <?php include '../footer.htm' ?>
