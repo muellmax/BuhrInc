493c06c8ea74aa96cc4f2127445c9f2119f2d936
Max Mueller <muellmax36@gmail.com>
2012-03-03 22:58:45 -0800

    updated login file to reflect login form.

--- login/index.php
+++ login/index.php
@@ -1,6 +1,16 @@
 <?php $pagetitle="Login/Logout"; ?>
 <?php include '../header.htm' ?>
 
-<p>You will be able to log in here.</p>
+<form id='login' action='login.php' method='post' accept-charset='UTF-8'>
+	<fieldset>
+		<legend>Login</legend>
+		<input type='hidden' name='submitted' id='submitted' value='1' />
+		<label for='username' >UserName*:</label>
+		<input type='text' name='username' id='username' maxlength="50" />
+		<label for='password' >Password*:</label>
+		<input type='password' name='password' id='password' maxlength="50" />
+		<input type='submit' name='Submit' value='Submit' />
+	</fieldset>
+</form>
 
 <?php include '../footer.htm' ?>
