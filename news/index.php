24d179aa18f60242ece75fe2d011bc7cf99920c9
Spencer Forell <forells@flip2.engr.oregonstate.edu>
2012-03-06 21:38:02 -0800

    commiting news, updated it by adding database interaction and dynamic creation of the web page

--- news/index.php
+++ news/index.php
@@ -1,18 +1,42 @@
 <?php $pagetitle="News"; ?>
 <?php include '../header.htm' ?>
 
-<p><b>Featured video: Daniel Johnston: An inspiration to many.</b></p>
 
-<object width="480" height="385" data="">
-<param name="movie" value="http://www.youtube.com/v/5GcWEupva5M?fs=1&amp;h1=en_US" />
-        <!-- Note: everything after the & was removed. -->
-<param name="allowFullScreen" value="true" />
-<param name="allowscriptaccess" value="always" />
-<embed src="http://www.youtube.com/v/5GcWEupva5M?fs=1&amp;h1=en_US"
-       type="application/x-shockwave-flash"
-       allowscriptaccess="always" allowfullscreen="true" width="480" height="385" />
-</object>
+<p><b>Here is where you will find news about any new artwork and sales!</b></p>
+
+
+<?php 
+	//define credentials
+	DEFINE ('DB_USER', 'forells-db');
+	DEFINE ('DB_PASSWORD', 'hvG5g5XxQUGSCdZP');
+	DEFINE ('DB_HOST', 'oniddb.cws.oregonstate.edu');
+	DEFINE ('DB_NAME', 'forells-db');
+
+	//establish a connection
+	$dbc = @mysql_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) 
+				  or die ('Connection to the database did not work because:  ' . mysql_connect_error() );
+				  
+	//select the database
+	mysql_select_db(DB_NAME, $dbc);
+
+	$sql = 'select News_ID, link, message, date_posted from News where active = 1';
+	$result = mysql_query($sql);
+	
+	echo 
+	"<table>
+		<tr>
+			<th>Picture</th>
+			<th>Message</th>
+		<tr>";
+	while($row = mysql_fetch_array($result)) {
+		echo "<tr>";
+		echo "<td><img src=" . $row['link'] . " height='100'/></td>";
+		echo "<td>" . $row['message'] . "</td>";
+		echo "</tr>";
+	}
+	echo "</table>";
+	mysql_close($con);
+?>
+
 
 <?php include '../footer.htm' ?>
-
-<!-- Video provided by you tube and posted by octogenerian. All songs and paintings by Daniel Johnston. -->
