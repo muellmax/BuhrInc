23d4be77c7050d09497966ed3648db96b0b55714
Phung Phan <phanp@onid.orst.edu>
2012-03-10 18:03:28 -0800

    updated connection.php and galleries/index.php

--- connection.php
+++ connection.php
@@ -1,16 +1,17 @@
-<?php   //connect to database example for PHP 5.2
+<?php
+	//define credentials
+	DEFINE ('DB_HOST', 'oniddb.cws.oregonstate.edu');
+	DEFINE ('DB_USER', 'forells-db');
+	DEFINE ('DB_PASSWORD', 'hvG5g5XxQUGSCdZP');
+	DEFINE ('DB_NAME', 'forells-db');
 
-//define credentials
-DEFINE ('DB_USER', 'forells-db');
-DEFINE ('DB_PASSWORD', 'hvG5g5XxQUGSCdZP');
-DEFINE ('DB_HOST', 'oniddb.cws.oregonstate.edu');
-DEFINE ('DB_NAME', 'forells-db');
+	//establish a connection
+	$connect = mysql_connect (DB_HOST, DB_USER, DB_PASSWORD) 
+		or die ("Unable to connect to server.<br>");
+	//echo "Connected to server " . DB_HOST . " as user " . DB_USER . "<BR>";
 
-//establish a connection
-$dbc = @mysql_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) 
-              or die ('Connection to the database did not work because:  ' . mysql_connect_error() );
-			  
-//select the database
-mysql_select_db(DB_NAME, $dbc);
-
+	//select the database
+	mysql_select_db (DB_NAME) 
+		or die ("Unable to select database" . DB_NAME . "<br>");
+	//echo "Connected to database " . DB_NAME . "<BR><BR>";
 ?>
--- galleries/index.php
+++ galleries/index.php
@@ -1,10 +1,18 @@
 <?php $pagetitle="Galleries"; ?>
-<?php include '../header.htm' ?>
+
+<?php
+	include '../header.htm';
+	include '../connection.php';
+
+	$query = "select * from Art";
+	$result = mysql_query ($query);
+?>
 
 <p>Check out these galleries. These picture are here until real content is provided by the client. </p>
 
 <figure id="gallery">
 	<ul class="gallery">
+	<!--
 		<li><a href=""><img src="../images/lastcomic.jpg" alt="A picture of Grace, Ava, and I at the Last Comic Standing show" title="&copy; Max Mueller 2010" /></a>
 			<ul>
 				<li>Ava, Grace and I at the Last Comic Standing show in Eugene, getting our picture taken with the winner Felipe Esparza.</li>
@@ -20,6 +28,30 @@
 				<li>My doggie Sugar and I.</li>
 			</ul>
 		</li>
+	-->
+
+<?php
+	while ($obj = mysql_fetch_object($result)) {
+		if ($obj->For_Sale == 0) { // not for sale
+?>
+		<li><a href=""><img src="<?php print($obj->Link);?>" title="Author: <?php print($obj->Author);?>" /></a>
+			<ul>
+				<li><?php print($obj->Description);?></li>
+			</ul>
+		</li>
+<?php
+		} else { // for sale; click on the image to purchase
+?>
+		<li><a href=""><img src="<?php print($obj->Link);?>" title="Author: <?php print($obj->Author);?>" /></a>
+			<ul>
+				<li><?php print($obj->Description); print("\n(Click to purchase)");?></li>
+			</ul>
+		</li>
+<?php
+		}
+	}
+?>
+
 	</ul>
 	<br style="clear: both;" />
 </figure>
