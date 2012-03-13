e56b7f61140967addd5a3429926194ddfa30ad48
Spencer Forell <forells@flip2.engr.oregonstate.edu>
2012-03-13 15:28:43 -0700

    adding new images and updating the contact and events sections

--- contact/sent.php
+++ contact/sent.php
@@ -4,32 +4,25 @@
 
 <?php 
 
-//define credentials
-DEFINE ('DB_USER', 'forells-db');
-DEFINE ('DB_PASSWORD', 'hvG5g5XxQUGSCdZP');
-DEFINE ('DB_HOST', 'oniddb.cws.oregonstate.edu');
-DEFINE ('DB_NAME', 'forells-db');
-
-//establish a connection
-$dbc = @mysql_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) 
-              or die ('Connection to the database did not work because:  ' . mysql_connect_error() );
-			  
-//select the database
-mysql_select_db(DB_NAME, $dbc);
-
 session_start();
 $userid = $_SESSION['user'];
 $subject = $_POST["subject"];
 $message = $_POST["message"];
 
-$sql = "insert into Email 
-		(Date_Sent, Subject, Message, Replied, User_ID) 
-		values
-		(NOW(), '" . $subject . "','" . $message . "', 0, " . $userid . ")";
-		
-mysql_query($sql);
-mysql_close($con);]
+if (is_null($userid)){
+	echo "Please login before sending a message!";
+}
 
+else {
+	$sql = "insert into Email 
+			(Date_Sent, Subject, Message, Replied, User_ID) 
+			values
+			(NOW(), '" . $subject . "','" . $message . "', 0, " . $userid . ")";
+			
+	mysql_query($sql);
+	//TODO: insert emailing here
+	echo "Information sent succesfully!"; 	
+}
 ?>
  
 <?php include '../footer.htm' ?>
