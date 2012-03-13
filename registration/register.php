62349226f4aee008dd5a3c5a45dddaee33d4c989
Max Mueller <muellmax36@gmail.com>
2012-03-13 15:16:54 -0700

    added registration error dup user checking

--- registration/register.php
+++ registration/register.php
@@ -1,5 +1,6 @@
 <?php
 //include 'http://people.oregonstate.edu/~muellmax/buhrinc/connection.php';
+session_start();
 ob_start();
 $dbuser = 'forells-db';
 $dbpass = 'hvG5g5XxQUGSCdZP';
@@ -29,6 +30,16 @@ echo $Fname;
 echo $Lname;
 echo $Email;
 echo $var;
+
+$select = mysql_query("SELECT Username from User WHERE Username = '" . $Uname . "'");
+
+$query_row = mysql_fetch_array($select); 
+if (!is_null($query_row[0])) {
+	$_SESSION['userdup'] = 1;
+	header('Location:http://people.oregonstate.edu/~muellmax/buhrinc/registration/index.php');
+	exit();
+}
+else $_SESSION['userdup'] = 0;
 
 $insert = mysql_query("INSERT INTO User (Username, Password, First_Name, Last_Name, Email)
 			 VALUES(\"$Uname\", \"$Pword\", \"$Fname\", \"$Lname\", \"$Email\")");
