404d66f192f4e6fee4591874ede31c03968e80b3
Max Mueller <muellmax36@gmail.com>
2012-03-12 23:22:05 -0700

    fixed css sheet. Started order form. buggy.

--- order_form/RegisterPaymentInfo.php
+++ order_form/RegisterPaymentInfo.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	ob_start();
-	$userID = $_SE
+	$userID = $_SESSION['user'];
 	
 	$dbuser = 'forells-db';
 	$dbpass = 'hvG5g5XxQUGSCdZP';
@@ -15,3 +15,41 @@
 	//select the database
 	mysql_select_db($dbname, $mysql_handle)
 		or die("Error selecting database: $dbname");
+		
+	$UserID = $_SESSION['user'];
+	if (is_null($UserID)) $_SESSION['error'] = 1;
+	$CardType = $_POST["cardtype"];
+	if (is_null($CardType)) $_SESSION['error'] = 1;
+	$CardNum = $_POST["cardnumber"];
+	if (is_null($CardNum)) $_SESSION['error'] = 1;
+	$BillAddress = $_POST["billaddress"];
+	if (is_null($BillAddress)) $_SESSION['error'] = 1;
+	$NameOnCard = $_POST["nameoncard"];
+	if (is_null($NameOnCard)) $_SESSION['error'] = 1;
+	$CardExpDate = $_POST["expdate"];
+	if (is_null($CardExpDate)) $_SESSION['error'] = 1;
+	$SecCode = $_POST["security"];
+	if (is_null($SecCode)) $_SESSION['error'] = 1;
+	
+	if ($_SESSION['error'] != 1) {
+		if ($_SESSION['flag'] == 0) {
+	
+			$query = mysql_query("INSERT INTO Payment_Info (User_ID, Cart_Type, Card_Number, Billing_Address, Name_On_Card, Card_Exp_Date, Security_Code)
+				VALUES(\"$UserID\", \"$CardType\", \"$CardNum\", \"$BillAddress\", \"$NameOnCard\", \"$CardExpDate\", \"$SecCode\")");
+		}
+		else {
+			$query = mysql_query("UPDATE Payment_Info
+								SET User_ID = $UserID, Cart_Type = $CardType,
+								Card_Number = $CardNum, Billing_Address = $BillAddress,
+								Name_On_Card = $NameOnCard, Card_Exp_Date = $CardExpDate,
+								Security_Code = $SecCode
+								WHERE User_ID = $UserID");
+		}
+		if (!$query) echo mysql_error();
+		echo $_SESSION['flag'];
+		echo "hi";
+		echo $_SESSION['error'];
+	}
+	mysql_close($mysql_handle);
+	//header('Location:http://people.oregonstate.edu/~muellmax/buhrinc/order_form/index.php');
+?>							  
--- order_form/index.php
+++ order_form/index.php
@@ -1,8 +1,14 @@
 <?php session_start(); ?>
-<?php $pagetitle="Login/Logout"; ?>
-<?php include '../header.htm' ?>
+<?php $pagetitle="Please provide payment information"; ?>
+<?php include '../header.htm'; ?>
 
 <?php
+
+	$dbuser = 'forells-db';
+	$dbpass = 'hvG5g5XxQUGSCdZP';
+	$dbhost = 'oniddb.cws.oregonstate.edu';
+	$dbname = 'forells-db';
+	
 	//establish a connection
 	$mysql_handle = @mysql_connect($dbhost, $dbuser, $dbpass)
 		or die("Error connecting to database server");
@@ -14,27 +20,56 @@
 	$userID = $_SESSION['user'];
 	
 	if (!is_null($userID)) {
-		$query = mysql_query("SELECT * FROM Payment_Info WHERE User_ID = '$userID' AND Password = '$Pword'");
+		$query = mysql_query("SELECT * FROM Payment_Info WHERE User_ID = '$userID'");
+		$query_row = mysql_fetch_array($query);
+		$cardType = $query_row[2];
+		$cardNumber = $query_row[3];
+		$billingAddress = $query_row[4];
+		$nameOnCard = $query_row[5];
+		$cardExpDt = $query_row[6];
+		$secCode = $query_row[7];
+		$_SESSION['flag'] = 1;
+	}
+	else {
+		$cardType = NULL;
+		$cardNumber = NULL;
+		$billingAddress = NULL;
+		$nameOnCard = NULL;
+		$cardExpDt = NULL;
+		$secCode = NULL;
+		$_SESSION['flag'] = 0;
 	}
 ?>
 
-<form id='RegisterUser' action='RegisterUser.php' method='post' accept-charset='UTF-8'>
+<form id='RegisterPaymentInfo' action='RegisterPaymentInfo.php' method='post' accept-charset='UTF-8'>
 	<fieldset>
 		<legend>Order form</legend>
 		<input type='hidden' name='submitted' id='submitted' value='1' />
 		<label for='cardtype' >Card Type:*</label>
-		<input type='text' name='cardtype' id='cardtype' maxlength="30" />
+		<input type='text' name='cardtype' id='cardtype' maxlength="30" value="<?php echo($cardType); ?>" />
 		<label for='cardnumber' >Credit Card Number:*</label>
-		<input type='text' name='cardnumber' id='cardnumber' maxlength="65" />
+		<input type='text' name='cardnumber' id='cardnumber' maxlength="65" value="<?php echo($cardNumber); ?>" />
 		<label for='billaddress' >Billing Address:*</label>
-		<input type='text' name='billaddress' id='billaddress' maxlength="65" />
+		<input type='text' name='billaddress' id='billaddress' maxlength="65" value="<?php echo($billingAddress); ?>" />
 		<label for='nameoncard' >Name On Card:*</label>
-		<input type='text' name='nameoncard' id='nameoncard' maxlength="65" />
+		<input type='text' name='nameoncard' id='nameoncard' maxlength="65" value="<?php echo($nameOnCard); ?>"/>
 		<label for='expdate' >Card Expiration Date*:</label>
-		<input type='text' name='expdate' id='expdate' maxlength="50" />
+		<input type='text' name='expdate' id='expdate' maxlength="50" value="<?php echo($cardExpDt); ?>"/>
 		<label for='security' >Security Code*:</label>
-		<input type='text' name='security' id='security' maxlength="50" />
+		<input type='text' name='security' id='security' maxlength="50" value="<?php echo($secCode); ?>"/>
 		<input type='submit' name='Submit' value='Submit' />
 	</fieldset>
 </form>
-<?php include '../footer.htm'; ?>
+
+<?php
+
+	if (isset($_SESSION['error'])) {
+		if ($_SESSION['error'] == 1) 
+			echo "<p>Please fill out all the required fields.</p>";
+		else 
+			echo "<p>You have successfully created payment information.</p>";
+	
+	}
+
+	include '../footer.htm'; 
+?>
