d5f9dd62fece8a01e1ff24e9aa6088f08c988a62
Max Mueller <muellmax36@gmail.com>
2012-03-14 23:45:06 -0700

    added verbage to indicate user is logged in after registering.

--- regConfirmation.php
+++ regConfirmation.php
@@ -1,6 +1,6 @@
 <?php $pagetitle="RegConfirmation"; ?>
 <?php include 'header.htm' ?>
 
-<p>Thank you! You have successfully created your account.</p>
+<p>Thank you! You have successfully created your account, and are currently logged in.</p>
 
 <?php include 'footer.htm' ?>
